bf70555a7207015396555940f44a6c9559635fe6
edgar0011 <martin.weiser@gmail.com>
2016-06-06 18:21:02 +0200

    initial commit after git ignored

---
+++ app/src/entry.js
@@ -0,0 +1,6 @@
+var moduleA = require('./modules/moduleA.js');
+var componentOne = require('./components/componentOne.js');
+document.write("Hello World");
+
+moduleA.mainMethod();
+componentOne.mainMethod();
